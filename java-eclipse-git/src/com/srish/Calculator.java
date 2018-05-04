c62a42e36be86c9d911910e8f8be5ca0608e511e
Training <srishtijn54@gmail.com>
2018-05-04 01:49:57 -0500

    Calculator V 1.0

---
+++ java-eclipse-git/src/com/srish/Calculator.java
@@ -0,0 +1,23 @@
+package com.srish;
+
+public class Calculator 
+{
+	Integer a,b;
+
+	public Integer getA() {
+		return a;
+	}
+
+	public void setA(Integer a) {
+		this.a = a;
+	}
+
+	public Integer getB() {
+		return b;
+	}
+
+	public void setB(Integer b) {
+		this.b = b;
+	}
+	
+}
